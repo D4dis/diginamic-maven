e6cff61d8a50f7dfa5bcd93ea7e4266ccf373c5f
Dadis <raditztv@gmail.com>
2025-07-25 16:22:13 +0200

    4th commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,14 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
         System.out.println("Hello World!");
     }
 }
